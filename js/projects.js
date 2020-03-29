dee99f687cef9707316355aad51ca81e380e7e7b
XroixHD <39132287+XroixHD@users.noreply.github.com>
2020-03-29 23:32:47 +0200

    New Design 🤨
     + Redesigned the whole page!
     + Beta Dark Mode :O
     + ...

--- js/projects.js
+++ js/projects.js
@@ -1,29 +1,52 @@
 /*
+MIT License
 
-    Work in progress ...
+Copyright (c) 2020 XroixHD
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 
-const PROJECTS = {"Cubameter": "xroix.me/Cubameter"};
+const PROJECTS = {"Cubameter": "https://www.xroix.me/Cubameter"};
+const PROJECTS_PREVIEW = {"Cubameter": "resources/projects/preview-cubameter.png"};
 const PROJECT_PARENT = document.getElementById("projects");
 
-// Add event listeners
-document.getElementsByClassName("project active")[0].addEventListener("click", active_project_click);
-var elements = document.getElementsByClassName("project blur");
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', blurred_project_click);
+// Initialize projects
+for (let i = 0; i < Object.keys(PROJECTS).length; i++) {
+    let name = Object.keys(PROJECTS)[i];
+    let e = document.createElement("div");
+    e.setAttribute("class", "project main");
+    e.innerHTML = _create_template(name, PROJECTS[name], PROJECTS_PREVIEW[name]);
+    PROJECT_PARENT.appendChild(e);
+    console.log(name);
+    console.log(e);
 }
 
-// event listeners
-function active_project_click () {
-    console.log("Active project was clicked!");
-    window.location = window.location + "/Cubameter"
+let elements_projects = document.getElementsByClassName("project main");
+for (let i = 0; i < elements_projects.length; i++) {
+    console.log(elements_projects[i]);
+    elements_projects[i].addEventListener("click", function click() {
+        let name = this.childNodes[0].innerHTML;
+        console.log(name + "second");
+        window.location = PROJECTS[name];
+    });
 }
 
-function blurred_project_click () {
-    console.log("Blurred project was clicked!");
-}
-
-function _create_template(name, url, preview, logo) {
-    return '<div class="project blur"><p class="project logo">' + name + '</p><img class="project preview" src="' + preview + '" alt="Cubetastic game!"><img class="project logo" src="' + logo + '" alt="Cubetastic game!"></div>';
+function _create_template(name, url, preview) {
+    return '<p class="project logo">' + name + '</p><img class="project preview" src="' + preview + '" alt="Cubetastic game!">';
 }
